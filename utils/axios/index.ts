abe757356da47b4459d425d39dd598b25b6d7a86
mohammed-masri <mqalmasri@sea.ac.ae>
2025-01-15 09:46:45 +0400

    use axios instance from the library

--- utils/axios/index.ts
+++ utils/axios/index.ts
@@ -1,36 +1,11 @@
 import { Constants } from "@/config";
 import { BACKEND_BASE_URL } from "@/config/env";
-import axios, { AxiosError, AxiosResponse } from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: BACKEND_BASE_URL,
+import { AxiosUtils } from "sea-react-components";
+
+const axiosInstance = AxiosUtils.createInstance(BACKEND_BASE_URL, {
+  JWTTokenKey: Constants.LocalStorageKeys.JWTToken,
+  withCredentials: true,
 });
-
-axiosInstance.defaults.withCredentials = true;
-
-// Request interceptor (if needed for adding authorization token, etc.)
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem(Constants.LocalStorageKeys.JWTToken);
-    config.headers["Authorization"] = `Bearer ${token}`;
-    return config;
-  },
-  (error) => {
-    // Handle request error
-    return Promise.reject(error);
-  }
-);
-
-// Response interceptor for handling success and errors globally
-axiosInstance.interceptors.response.use(
-  (response: AxiosResponse) => {
-    return response.data;
-  },
-  (error: AxiosError) => {
-    console.log(`Error: ${error.message}`);
-
-    return Promise.reject(error.response?.data);
-  }
-);
 
 export default axiosInstance;
